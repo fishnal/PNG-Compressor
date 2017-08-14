3f9d28466802c6c9e24fa54f9218166f274637d9
Vishal Patel <patelvishal3199@gmail.com>
2017-08-14 10:17:31 -0400

    Added tests and cmd-line arguments
    + Test images and outputs added, see "tests" folder
    + Command line arguments added
      + Multi-core is disabled by default and can be enabled by applying the "mc" or "multi-core" option (no value can be assigned to this option from the user)
      + Replaced some areas of the code to make it neater
      + Changed some exit codes
      + Made it slightly more verbose
    + Jar packaging updated and now includes source file

--- PNGCompressor.java
+++ PNGCompressor.java
@@ -3,57 +3,134 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PNGCompressor {
-	public static void main(String[] args) throws Exception {
-		if (args.length != 2) {
-			System.out.println("usage: java -jar pngc.jar [input directory] [output directory]");
-			System.exit(1);
-		}
+	private final static Map<String, String[]> OPTIONS_MAP = new HashMap<>();
+	private final static Map<String, String> OPTIONS_VALUES = new HashMap<>();
+	private final static String formatter;
+	private final static String[] EMPTY_VALUES = new String[0];
+	private final static String EMPTY_VALUE = "";
 
-		// retrieve pngquant.exe from the jar file, store as a temporary executable file as 
+	static {
+        OPTIONS_MAP.put("multi-core", EMPTY_VALUES);
+        OPTIONS_MAP.put("mc", OPTIONS_MAP.get("multi-core"));
+        StringBuilder fsb = new StringBuilder("%-0s");
+        OPTIONS_MAP.forEach((k, v) -> {
+            OPTIONS_VALUES.put(k, null);
+            int len = k.length();
+            int a = fsb.indexOf("-") + 1;
+            int b = fsb.indexOf("s");
+            int currLen = Integer.parseInt(fsb.subSequence(a, b).toString());
+            if (len > currLen) {
+                fsb.replace(a, b, "" + len + "");
+            }
+        });
+        formatter = fsb.toString() + " %s\n";
+    }
+
+	private static boolean verifyArguments(String[] args) {
+		boolean bad = false;
+		if (args.length < 2) {
+			bad = true;
+		} else {
+			String in = args[args.length - 2];
+			String out = args[args.length - 1];
+			if (OPTIONS_MAP.containsKey(in) || OPTIONS_MAP.containsKey(out)) {
+				bad = true;
+			} else {
+				mainLoop : for (int i = 0; i < args.length - 2; i += 2) {
+					String option = args[i];
+					if (!option.startsWith("--")) {
+						System.out.println("invalid start of option: use \"--option-name\"");
+						return false;
+					}
+					String[] validValues = OPTIONS_MAP.get(option = option.substring(2));
+					if (validValues == null) {
+						System.out.println(String.format("option \"%s\" does not exist", option));
+						return false;
+					}
+					if (validValues != EMPTY_VALUES) {
+						if (args[i + 1].equals(in)) {
+							System.out.println(String.format("no value set for option \"%s\"", option));
+							return false;
+						}
+						String value = args[i + 1];
+						for (String vv : validValues) {
+							if (!vv.equals(value)) continue;
+							OPTIONS_VALUES.put(option, value);
+							continue mainLoop;
+						}
+						System.out.println(String.format("value \"%s\" is not valid for option \"%s\"", value, option));
+						continue;
+					}
+					OPTIONS_VALUES.put(option, EMPTY_VALUE);
+					i--;
+				}
+			}
+		}
+		if (bad) {
+			String usage = "usage: java -jar pngc.jar <options> [input directory] [output directory]\n";
+			StringBuilder osb = new StringBuilder(String.format(formatter, "options", "values"));
+			OPTIONS_MAP.forEach((k, v) -> {
+				String vs = java.util.Arrays.toString(v);
+				vs = vs.substring(1, vs.length() - 1);
+				if ((vs = vs.replace(", ", "|")).isEmpty()) {
+					vs = "NO VALUE NEEDED";
+				}
+				osb.append(String.format(formatter, k, vs));
+			});
+			String options = osb.toString();
+			System.out.println(usage + options);
+			return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws Exception {
+		if (!verifyArguments(args)) {
+			System.exit(2);
+		}
 		File PNGQUANT_PATH = new File("pngquant.exe");
 		PNGQUANT_PATH.deleteOnExit();
-		FileOutputStream fos = new FileOutputStream(PNGQUANT_PATH);
-		InputStream is = PNGCompressor.class.getResourceAsStream("/pngquant.exe");
-		pipe(is, fos);
-		is.close();
-		fos.close();
-
-		File dir = new File(args[0]);
+		{
+			FileOutputStream pngquantOS = new FileOutputStream(PNGQUANT_PATH);
+			InputStream pngquantIS = PNGCompressor.class.getResourceAsStream("/pngquant.exe");
+			pipe(pngquantIS, pngquantOS);
+			pngquantIS.close();
+			pngquantOS.close();
+		}
+		File dir = new File(args[args.length - 2]);
+		if (!dir.exists()) {
+			System.out.println(dir + " does not exist");
+			System.exit(1);
+		}
+		int numberOfCores = OPTIONS_VALUES.get("multi-core") != null || OPTIONS_VALUES.get("mc") != null ? Runtime.getRuntime().availableProcessors() : 1;
 		File[] files = dir.listFiles();
-		for (File f : files) {
-			String name = f.getName();
-			if (!name.endsWith(".png") || !f.isFile()) {
-				continue;
-			}
-			File out = new File(args[1]);
+		for (File f : dir.listFiles()) {
+			String string = f.getName();
+			if (!string.endsWith(".png") || !f.isFile()) continue;
+			File out = new File(args[args.length - 1]);
 			out.mkdirs();
-			out = new File(out + "/" + name);
-
-			while (Thread.activeCount() >= 1 + Runtime.getRuntime().availableProcessors());
-
+			out = new File(out + "/" + string);
+			while (Thread.activeCount() >= 1 + numberOfCores) {
+			}
 			String command = String.format("\"%s\" --force --quality 50-50 \"%s\" --output \"%s\"", PNGQUANT_PATH, f, out);
 			new Thread(() -> {
 				try {
-					Process p = Runtime.getRuntime().exec(command);
-					p.waitFor();
-					if (p.exitValue() != 0) {
-						InputStream es = p.getErrorStream();
-						int data;
-						while ((data = es.read()) != -1) {
-							System.out.write(data);
-							System.out.flush();
-						}
-						System.exit(2);
+					Process process = Runtime.getRuntime().exec(command);
+					process.waitFor();
+					if (process.exitValue() != 0) {
+						pipe(process.getErrorStream(), System.out);
+						System.exit(3);
 					}
-				} catch (Exception e) {
-					e.printStackTrace(System.err);
-					System.exit(3);
 				}
-
-				System.out.println(f);
+				catch (Exception e) {
+					e.printStackTrace(System.err);
+					System.exit(4);
+				}
+				System.out.println("COMPRESSED\t" + f);
 			}).start();
 		}
 	}
